b23f97f0c17d0c02fa502f39f14b0adf30ef0fba
Elwin Luo <elwin.luo@tradehero.mobi>
2017-11-29 15:28:06 +0800

    add samples in api_usage.py

--- api_usage.py
+++ api_usage.py
@@ -1,8 +1,9 @@
 from wrapi.order import OrderStyle, get_open_orders, order_target, order_target_percent, cancel_order, set_stop_price
 from wrapi.context import Context
+from wrapi.data import Data
 
 if __name__ == '__main__':
-    print get_open_orders()
+    # print get_open_orders()
     # print get_open_orders('UBT')
     # order_target('SPY', 10, OrderStyle.LimitOrder(260))
     # order_target('QQQ', 12)
@@ -11,16 +12,21 @@ if __name__ == '__main__':
     # order_target('SPY', -6, style=OrderStyle.LimitOrder(258.0))
     # cancel_order(100069)
     # set_stop_price('SPY', 250)
-    context = Context()
-    print context.portfolio.portfolio_value
-    print context.portfolio.positions_value
-    print context.portfolio.capital_used
-    print context.portfolio.positions
-    print context.portfolio.positions['SPY'].symbol
-    print context.portfolio.positions['SPY'].amount
-    print context.portfolio.positions['SPY'].last_sale_price
-    print context.portfolio.positions['SPY'].cost_basic
+    # context = Context()
+    # print context.portfolio.portfolio_value
+    # print context.portfolio.positions_value
+    # print context.portfolio.capital_used
+    # print context.portfolio.positions
+    # print context.portfolio.positions['SPY'].symbol
+    # print context.portfolio.positions['SPY'].amount
+    # print context.portfolio.positions['SPY'].last_sale_price
+    # print context.portfolio.positions['SPY'].cost_basic
     # context.abc = 1
     # print context.abc
+    data = Data()
+    # print data.history('QQQ', field='close', window=100)
+    # print data.history(['SPY', 'VIX'], window=252)
+    print data.current(['SPY', 'VIX'])
+
 
 
