fccc6143b32e03f2e19f9947b82b9d49a98bc945
Elwin Luo <elwin.luo@tradehero.mobi>
2017-12-01 15:11:14 +0800

    add date rules test cases, add time rules

--- tests/date_rules_test.py
+++ tests/date_rules_test.py
@@ -10,3 +10,32 @@ class DateRulesTest(unittest.TestCase):
         self.assertFalse(date_rules.everyday().validate(datetime.date(2017, 11, 23)))
         self.assertFalse(date_rules.everyday().validate(datetime.date(2017, 11, 25)))
 
+    def test_week_start(self):
+        self.assertTrue(date_rules.week_start().validate(datetime.date(2017, 11, 27)))
+        self.assertTrue(date_rules.week_start(days_offset=1).validate(datetime.date(2017, 11, 28)))
+        self.assertTrue(date_rules.week_start(days_offset=3).validate(datetime.date(2017, 11, 30)))
+        self.assertTrue(date_rules.week_start(days_offset=-1).validate(datetime.date(2017, 11, 24)))
+        self.assertTrue(date_rules.week_start(days_offset=-2).validate(datetime.date(2017, 11, 22)))
+        self.assertFalse(date_rules.week_start(days_offset=-2).validate(datetime.date(2017, 11, 23)))
+
+    def test_week_end(self):
+        self.assertTrue(date_rules.week_end().validate(datetime.date(2017, 11, 24)))
+        self.assertTrue(date_rules.week_end(-1).validate(datetime.date(2017, 11, 22)))
+        self.assertTrue(date_rules.week_end(+1).validate(datetime.date(2017, 11, 27)))
+
+    def test_month_start(self):
+        self.assertTrue(date_rules.month_start().validate(datetime.date(2017, 11, 1)))
+        self.assertFalse(date_rules.month_start().validate(datetime.date(2017, 10, 1)))
+        self.assertTrue(date_rules.month_start(1).validate(datetime.date(2017, 11, 2)))
+        self.assertTrue(date_rules.month_start(7).validate(datetime.date(2017, 11, 10)))
+        self.assertTrue(date_rules.month_start(-1).validate(datetime.date(2017, 11, 30)))
+
+    def test_month_end(self):
+        self.assertTrue(date_rules.month_end().validate(datetime.date(2017, 11, 30)))
+        self.assertTrue(date_rules.month_end().validate(datetime.date(2017, 10, 31)))
+        self.assertFalse(date_rules.month_end().validate(datetime.date(2017, 9, 30)))
+        self.assertTrue(date_rules.month_end().validate(datetime.date(2017, 9, 29)))
+        self.assertTrue(date_rules.month_end(-1).validate(datetime.date(2017, 9, 28)))
+        self.assertTrue(date_rules.month_end(2).validate(datetime.date(2017, 10, 3)))
+
+
---
+++ wrapi/time_rules.py
@@ -0,0 +1,78 @@
+from abc import abstractmethod, ABCMeta
+from common.tradetime import TradeTime
+
+
+class TimeRule(object):
+
+    @abstractmethod
+    def validate(self, dt):
+        pass
+
+    @staticmethod
+    def check_parameters(hours, minutes):
+        if hours * 60 + minutes >= 6 * 60 + 30:
+            raise Exception('Invalid parameters')
+
+
+class MarketOpenRule(TimeRule):
+
+    def __init__(self, hours=0, minutes=1):
+        TimeRule.check_parameters(hours, minutes)
+        self.hours = hours
+        self.minutes = minutes
+
+    def validate(self, dt):
+        if TradeTime.is_trade_day(dt.date()):
+            if TradeTime.is_half_trade_day(dt.date()) and self.hours * 60 + self.minutes >= 3*60 + 30:
+                return False
+            else:
+                passed_minutes = dt.hour * 60 + dt.minute - (9*60 + 30)
+                if passed_minutes == self.hours * 60 + self.minutes:
+                    return True
+                else:
+                    return False
+        return False
+
+
+class MarketCloseRule(TimeRule):
+
+    def __init__(self, hours=0, minutes=1):
+        TimeRule.check_parameters(hours, minutes)
+        self.hours = hours
+        self.minutes = minutes
+
+    def validate(self, dt):
+        if TradeTime.is_trade_day(dt.date()):
+            if TradeTime.is_half_trade_day(dt.date()):
+                if self.hours * 60 + self.minutes >= 3.5*60:
+                    return False
+                else:
+                    left_minutes = 13 * 60 - (dt.hour * 60 + dt.minute)
+                    if left_minutes == self.hours * 60 + self.minutes:
+                        return True
+                    else:
+                        return False
+
+            else:
+                left_minutes = 16 * 60 - (dt.hour * 60 + dt.minute)
+                if left_minutes == self.hours * 60 + self.minutes:
+                    return True
+                else:
+                    return False
+        return False
+
+
+class time_rules(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def market_open(hours=0, minutes=1):
+        return MarketOpenRule(hours, minutes)
+
+    @staticmethod
+    def market_close(hours=0, minutes=1):
+        return MarketCloseRule(hours, minutes)
+
+
